0d5a6930df7b2471523589e87fff34366855c956
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-04-10 17:40:06 -0700

    Add compiler_warnings

---
+++ cmake/compiler_warnings.cmake
@@ -0,0 +1,37 @@
+function(set_project_warnings project_name)
+    set(CLANG_WARNINGS
+        -Wall
+        -Wextra
+        -Wshadow
+        -Wnon-virtual-dtor
+        -Wold-style-cast
+        -Wcast-align
+        -Wunused
+        -Woverloaded-virtual
+        -Wpedantic
+        -Wconversion
+        -Wsign-conversion
+        -Wnull-dereference
+        -Wdouble-promotion
+        -Wformat=2)
+
+    set(GCC_WARNINGS
+        ${CLANG_WARNING}
+        -Wmisleading-indentation
+        -Wduplicated-cond
+        -Wduplicated-branches
+        -Wlogical-op
+        -Wuseless-cast
+        )
+
+    if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+        set(PROJECT_WARNINGS ${CLANG_WARNINGS})
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        set(PROJECT_WARNINGS ${GCC_WARNINGS})
+    endif()
+
+    target_compile_options(${project_name} INTERFACE ${PROJECT_WARNINGS})
+
+    message("Set compiler warnings successfully")
+
+endfunction()
